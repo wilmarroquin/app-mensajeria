b954fb654b0f9f97b0a3b3ce89d894a99accf4a8
BAlvarez12 <balvarezo3@miumg.edu.gt>
2025-02-20 22:37:46 -0600

    Primera version

---
+++ app/src/main/java/com/example/app_mensajeria/ListaChatsActivity.java
@@ -0,0 +1,12 @@
+package com.example.app_mensajeria;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListaChatsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_chats);
+    }
+}
---
+++ app/src/main/java/com/example/app_mensajeria/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.app_mensajeria;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListaChatsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
